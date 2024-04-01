5f632ad791e412c74711b4645edf6bcc3a06cac9
Pratik Thorawade <86850437+Pratik-20@users.noreply.github.com>
2024-04-01 06:41:33 +0530

    Update greet_time_tracker.py

--- greet_time_tracker.py
+++ greet_time_tracker.py
@@ -2,14 +2,17 @@
 
 def main():
     # Your existing code here (e.g., print("Hello, World!"))
-
+    print("Opened Code") 
     # Get the current date and time
     #now = datetime.datetime.now()
-
+    print("Running Code")
     # Write to the text file
     with open("output.txt", "a") as file:
         file.write(f"Hello, World!") 
         #file.write(f"Hello, World! ({now:%Y-%m-%d %H:%M:%S})\n")
-
+    print("Finished Code")
+    print("Finished Code")
+    print("Finished Code")
+    print("Finished Code...!")
 if __name__ == "__main__":
     main()
